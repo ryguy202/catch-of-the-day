d8dadae648b1a5377ee06b8363466339c9aa8d73
Ryan Garritson <ry.garritson777@gmail.com>
2018-07-17 20:25:57 -0600

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <div ClassName="header">
+        <header className="top">
+          <h1>
+            Catch
+            <span className="ofThe">
+              <span className="of">of</span>
+              <span className="the">the</span>
+            </span>
+            Day
+          </h1>
+          <h3 className="tagline">
+            <span>Fresh Daily</span>
+          </h3>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Inventory.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Inventory extends Component {
+  render() {
+    return <div className="in">Inventory</div>;
+  }
+}
+
+export default Inventory;
